8239e9b628fbeb480a2367724d25240e3598015d
lufeil <3510002513@qq.com>
2019-03-07 23:52:54 +0800

    String、StringBuffer、StringBuilder

---
+++ coreJava/src/main/java/java36/c1/s5/Demo01.java
@@ -0,0 +1,20 @@
+package java36.c1.s5;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        String str = "hello,world!";
+        char[] chars = new char[4];
+        byte[] bytes = new byte[4];
+        chars[0] = 'h';
+        chars[1] = 'l';
+        chars[2] = 'l';
+        chars[3] = 'l';
+        bytes[0] = 'h';
+        bytes[1] = 'l';
+        bytes[2] = 'l';
+        bytes[3] = 'l';
+        System.out.println(new String(chars));
+        System.out.println(new String(bytes));
+
+    }
+}
