0c5896c4cfb5140befd7e6543fe4bc503362fa8a
lufeil <3510002513@qq.com>
2019-03-18 23:02:16 +0800

    reflect&proxy-InvocationHandler

---
+++ coreJava/src/main/java/java36/c1/s06/Demo01.java
@@ -0,0 +1,20 @@
+package java36.c1.s06;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class Demo01 {
+    Class<String> c = null;
+    Field field = null;
+    Method method = null;
+    Constructor constructor = null;
+
+    private void test() throws Exception{
+//        AccessibleObject.setAccessible(flase);
+    }
+    public static void main(String[] args) {
+
+    }
+}
