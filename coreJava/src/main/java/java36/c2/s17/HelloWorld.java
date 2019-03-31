c4b30b66534ab9ac492d59633a303348a65190a0
lufeil <3510002513@qq.com>
2019-03-31 15:20:44 +0800

    thread.start()、thread.State

---
+++ coreJava/src/main/java/java36/c2/s17/HelloWorld.java
@@ -0,0 +1,36 @@
+package java36.c2.s17;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+
+
+        ThreadGroup group = Thread.currentThread().getThreadGroup();
+        ThreadGroup topGroup = group;
+        // 遍历线程组树，获取根线程组
+        while (group != null) {
+            topGroup = group;
+            group = group.getParent();
+        }
+        // 激活的线程数加倍
+        int estimatedSize = topGroup.activeCount() * 2;
+        Thread[] slackList = new Thread[estimatedSize];
+        // 获取根线程组的所有线程
+        int actualSize = topGroup.enumerate(slackList);
+        // copy into a list that is the exact size
+        Thread[] list = new Thread[actualSize];
+        System.arraycopy(slackList, 0, list, 0, actualSize);
+        System.out.println("Thread list size == " + list.length);
+        for (Thread thread : list) {
+            System.out.println(thread.getName());
+        }
+
+        // 获取线程数
+        ThreadGroup threadGroup = Thread.currentThread().getThreadGroup();
+        while(threadGroup.getParent() != null){
+            threadGroup = threadGroup.getParent();
+        }
+        int totalThread = threadGroup.activeCount();
+        System.out.println(totalThread);
+    }
+}
